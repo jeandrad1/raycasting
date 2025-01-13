04a2b4714aa258eed3580d6af36145e1eb7b974a
jeandrad1 <jesusandradeperez@gmail.com>
2025-01-13 18:21:07 +0100

    drawing.c and key_a.c now respect norminette

--- drawing.c
+++ drawing.c
@@ -6,42 +6,41 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 12:25:23 by jeandrad          #+#    #+#             */
-/*   Updated: 2024/12/13 19:25:23 by jeandrad         ###   ########.fr       */
+/*   Updated: 2025/01/13 18:20:34 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-// Función para limpiar la pantalla
-void clear_image(mlx_image_t *image, uint32_t color)
+// Function to clean the image
+void	clear_image(mlx_image_t *image, uint32_t color)
 {
-    int x;
-    int y;
+	int	x;
+	int	y;
 
-    y = 0;
-    x = 0;
-    while (y < SCREENHEIGHT)
-    {
-        x = 0;
-        while  (x < SCREENWIDTH)
-        {
-            mlx_put_pixel(image, x, y, color);
-            x++;
-        }
-        y++;
-    }
+	y = 0;
+	x = 0;
+	while (y < SCREENHEIGHT)
+	{
+		x = 0;
+		while (x < SCREENWIDTH)
+		{
+			mlx_put_pixel(image, x, y, color);
+			x++;
+		}
+		y++;
+	}
 }
 
-// Función para dibujar una línea
-void draw_line(t_game *game, int x, int start, int end, uint32_t color)
+// Function to draw a line
+void	draw_line(t_game *game, int x, int start, int end, uint32_t color)
 {
-    int y;
+	int	y;
 
-    y = start;
-    while (y < end)
-    {
-        mlx_put_pixel(game->image, x, y, color);
-        y++;
-    }
+	y = start;
+	while (y < end)
+	{
+		mlx_put_pixel(game->image, x, y, color);
+		y++;
+	}
 }
-
--- key_a.c
+++ key_a.c
@@ -6,53 +6,57 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 18:15:23 by jeandrad          #+#    #+#             */
-/*   Updated: 2025/01/13 18:15:42 by jeandrad         ###   ########.fr       */
+/*   Updated: 2025/01/13 18:19:57 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "cub3d.h"
 
-void	move_left_planeX(t_game *game, double moveSpeed)
+// Aux function to move the player to the left
+// in the X axis
+void	move_left_plane_x(t_game *game, double moveSpeed)
 {
-    if (game->worldMap[(int)(game->posY)][(int)(game->posX - game->planeX *
-		moveSpeed + PLAYER_RADIUS)] == '0'
-        && game->worldMap[(int)(game->posY)][(int)(game->posX - game->planeX *
-		moveSpeed - PLAYER_RADIUS)] == '0'
-        && game->worldMap[(int)(game->posY + PLAYER_RADIUS)][(int)(game->posX - game->planeX *
-		moveSpeed)] == '0'
-        && game->worldMap[(int)(game->posY - PLAYER_RADIUS)][(int)(game->posX - game->planeX *
-		moveSpeed)] == '0')
-    {
-        game->posX -= game->planeX * moveSpeed;
-    }
+	if (game->worldMap[(int)(game->posY)][(int)(game->posX - game->planeX
+			* moveSpeed + PLAYER_RADIUS)] == '0'
+		&& game->worldMap[(int)(game->posY)][(int)(game->posX - game->planeX
+			* moveSpeed - PLAYER_RADIUS)] == '0'
+		&& game->worldMap[(int)(game->posY + PLAYER_RADIUS)][(int)(game->posX
+			- game->planeX * moveSpeed)] == '0'
+		&& game->worldMap[(int)(game->posY - PLAYER_RADIUS)][(int)(game->posX
+			- game->planeX * moveSpeed)] == '0')
+	{
+		game->posX -= game->planeX * moveSpeed;
+	}
 }
 
-void	move_left_planeY(t_game *game, double moveSpeed)
+// Aux function to move the player to the left
+// in the Y axis
+void	move_left_plane_y(t_game *game, double moveSpeed)
 {
-    if (game->worldMap[(int)(game->posY - game->planeY *
-		moveSpeed + PLAYER_RADIUS)][(int)(game->posX)] == '0'
-        && game->worldMap[(int)(game->posY - game->planeY *
-		moveSpeed - PLAYER_RADIUS)][(int)(game->posX)] == '0'
-        && game->worldMap[(int)(game->posY - game->planeY *
-		moveSpeed)][(int)(game->posX + PLAYER_RADIUS)] == '0'
-        && game->worldMap[(int)(game->posY - game->planeY *
-		moveSpeed)][(int)(game->posX - PLAYER_RADIUS)] == '0')
-    {
-        game->posY -= game->planeY * moveSpeed;
-    }
+	if (game->worldMap[(int)(game->posY - game->planeY * moveSpeed
+			+ PLAYER_RADIUS)][(int)(game->posX)] == '0'
+		&& game->worldMap[(int)(game->posY - game->planeY * moveSpeed
+			- PLAYER_RADIUS)][(int)(game->posX)] == '0'
+		&& game->worldMap[(int)(game->posY - game->planeY
+			* moveSpeed)][(int)(game->posX + PLAYER_RADIUS)] == '0'
+		&& game->worldMap[(int)(game->posY - game->planeY
+			* moveSpeed)][(int)(game->posX - PLAYER_RADIUS)] == '0')
+	{
+		game->posY -= game->planeY * moveSpeed;
+	}
 }
 
+// Function to move the player to the left
 void	key_a(mlx_key_data_t keydata, void *param)
 {
-    double	moveSpeed;
-    t_game	*game;
+	double	move_speed;
+	t_game	*game;
 
-    moveSpeed = MOVE_SPEED;
-    game = (t_game *)param;
-    if (keydata.key == MLX_KEY_A)
-    {
-        move_left_planeX(game, moveSpeed);
-        move_left_planeY(game, moveSpeed);
-    }
+	move_speed = MOVE_SPEED;
+	game = (t_game *)param;
+	if (keydata.key == MLX_KEY_A)
+	{
+		move_left_plane_x(game, move_speed);
+		move_left_plane_y(game, move_speed);
+	}
 }
