7572e110ddad05a87951cccd65cac1987279a05e
jeandrad1 <jesusandradeperez@gmail.com>
2025-01-13 18:33:03 +0100

    textures.c and turn_mov.c now respect norminette

--- textures.c
+++ textures.c
@@ -1,34 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   textures.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 18:30:26 by jeandrad          #+#    #+#             */
+/*   Updated: 2025/01/13 18:30:33 by jeandrad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-void error(const char *message, t_game *game)
+void	error(const char *message, t_game *game)
 {
-    fprintf(stderr, "Error: %s\n", message);
-    free_map(game->worldMap, game->mapHeight);
-    exit(1);
+	fprintf(stderr, "Error: %s\n", message);
+	free_map(game->worldMap, game->mapHeight);
+	exit(1);
 }
 
-void load_walls(t_game *game)
+void	load_walls(t_game *game)
 {
-    xpm_t *xpm;
+	xpm_t	*xpm;
 
-    xpm = mlx_load_xpm42("textures/bluestone.xpm42");
-    if (!xpm)
-        error("texture not found", game);
-    game->textures.north = &xpm->texture;
-
-    xpm = mlx_load_xpm42("textures/eagle.xpm42");
-    if (!xpm)
-        error("texture not found", game);
-    game->textures.south = &xpm->texture;
-
-    xpm = mlx_load_xpm42("textures/mossy.xpm42");
-    if (!xpm)
-        error("texture not found", game);
-    game->textures.west = &xpm->texture;
-
-    xpm = mlx_load_xpm42("textures/redbrick.xpm42");
-    if (!xpm)
-        error("texture not found", game);
-    game->textures.east = &xpm->texture;
+	xpm = mlx_load_xpm42("textures/bluestone.xpm42");
+	if (!xpm)
+		error("texture not found", game);
+	game->textures.north = &xpm->texture;
+	xpm = mlx_load_xpm42("textures/eagle.xpm42");
+	if (!xpm)
+		error("texture not found", game);
+	game->textures.south = &xpm->texture;
+	xpm = mlx_load_xpm42("textures/mossy.xpm42");
+	if (!xpm)
+		error("texture not found", game);
+	game->textures.west = &xpm->texture;
+	xpm = mlx_load_xpm42("textures/redbrick.xpm42");
+	if (!xpm)
+		error("texture not found", game);
+	game->textures.east = &xpm->texture;
 }
-
--- turn_mov.c
+++ turn_mov.c
@@ -6,37 +6,46 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 17:58:52 by jeandrad          #+#    #+#             */
-/*   Updated: 2025/01/13 17:59:08 by jeandrad         ###   ########.fr       */
+/*   Updated: 2025/01/13 18:32:20 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void turn_left(t_game *game, double rotSpeed)
+void	turn_left(t_game *game, double rotSpeed)
 {
-    t_old old = {game->dirX, game->planeX};
-    game->dirX = game->dirX * cos(-rotSpeed) - game->dirY * sin(-rotSpeed);
-    game->dirY = old.dirX * sin(-rotSpeed) + game->dirY * cos(-rotSpeed);
-    game->planeX = game->planeX * cos(-rotSpeed) - game->planeY * sin(-rotSpeed);
-    game->planeY = old.planeX * sin(-rotSpeed) + game->planeY * cos(-rotSpeed);
+	t_old	old;
+
+	old.dirX = game->dirX;
+	old.planeX = game->planeX;
+	game->dirX = game->dirX * cos(-rotSpeed) - game->dirY * sin(-rotSpeed);
+	game->dirY = old.dirX * sin(-rotSpeed) + game->dirY * cos(-rotSpeed);
+	game->planeX = game->planeX * cos(-rotSpeed) - game->planeY
+		* sin(-rotSpeed);
+	game->planeY = old.planeX * sin(-rotSpeed) + game->planeY * cos(-rotSpeed);
 }
 
-void turn_right(t_game *game, double rotSpeed)
+void	turn_right(t_game *game, double rotSpeed)
 {
-    t_old old = {game->dirX, game->planeX};
-    game->dirX = game->dirX * cos(rotSpeed) - game->dirY * sin(rotSpeed);
-    game->dirY = old.dirX * sin(rotSpeed) + game->dirY * cos(rotSpeed);
-    game->planeX = game->planeX * cos(rotSpeed) - game->planeY * sin(rotSpeed);
-    game->planeY = old.planeX * sin(rotSpeed) + game->planeY * cos(rotSpeed);
+	t_old	old;
+
+	old.dirX = game->dirX;
+	old.planeX = game->planeX;
+	game->dirX = game->dirX * cos(rotSpeed) - game->dirY * sin(rotSpeed);
+	game->dirY = old.dirX * sin(rotSpeed) + game->dirY * cos(rotSpeed);
+	game->planeX = game->planeX * cos(rotSpeed) - game->planeY * sin(rotSpeed);
+	game->planeY = old.planeX * sin(rotSpeed) + game->planeY * cos(rotSpeed);
 }
 
-void turn_mov(mlx_key_data_t keydata, void *param)
+void	turn_mov(mlx_key_data_t keydata, void *param)
 {
-    t_game *game = (t_game *)param;
-    double rotSpeed = ROT_SPEED;
+	t_game	*game;
+	double	rot_speed;
 
-    if (keydata.key == MLX_KEY_LEFT)
-        turn_left(game, rotSpeed);
-    if (keydata.key == MLX_KEY_RIGHT)
-        turn_right(game, rotSpeed);
+	game = (t_game *)param;
+	rot_speed = ROT_SPEED;
+	if (keydata.key == MLX_KEY_LEFT)
+		turn_left(game, rot_speed);
+	if (keydata.key == MLX_KEY_RIGHT)
+		turn_right(game, rot_speed);
 }
